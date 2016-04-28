83cdb248afc0cbbf0188e2536ca522a5d01dc3f2
yonvaida <yon_vaida@yahoo.com>
2016-04-28 12:40:06 +0300

    Verify if number is in correct interval

---
+++ JuniorMindProblems/ConvertToRoman/RomanNumeral.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConvertToRoman
+{
+    [TestClass]
+    public class RomanNumeral
+    {
+        [TestMethod]
+        public void ValidNumber()
+        {
+            Assert.AreEqual("Are in interval", RomanConverted(50));
+        }
+        string RomanConverted(int number)
+        {
+            if (number < 0 && number > 100)
+            {
+                return "Not in interval";
+            }else {
+                return "Are in interval";
+            }
+        }
+    }
+}
