d03049ef6575e50368a77c7fdf5dd6b49dd127cd
yonvaida <yon_vaida@yahoo.com>
2016-04-29 09:07:58 +0300

    fara recursivitate si conditii
    Am facut teste si pt numerele sub 10 si pt numerele multiplu de 10

--- JuniorMindProblems/ConvertToRoman/RomanNumeral.cs
+++ JuniorMindProblems/ConvertToRoman/RomanNumeral.cs
@@ -16,10 +16,33 @@ namespace ConvertToRoman
         [TestMethod]
         public void convertNumberTest()
         {
-            Assert.AreEqual("XVIII", convertNumber(18));
+            Assert.AreEqual("XXI", convertNumber(21));
         }
-
-        
+        [TestMethod]
+        public void convertNumberTestfor10()
+        {
+            Assert.AreEqual("X", convertNumber(10));
+        }
+        [TestMethod]
+        public void convertNumberTestfor1()
+        {
+            Assert.AreEqual("I", convertNumber(1));
+        }
+        [TestMethod]
+        public void convertNumberTestfor5()
+        {
+            Assert.AreEqual("V", convertNumber(5));
+        }
+        [TestMethod]
+        public void convertNumberTestfor23()
+        {
+            Assert.AreEqual("XXIII", convertNumber(23));
+        }
+        [TestMethod]
+        public void convertNumberTestfor100()
+        {
+            Assert.AreEqual("C", convertNumber(100));
+        }
 
         string validNumber(int number)
         {
@@ -33,15 +56,14 @@ namespace ConvertToRoman
         }
          string convertNumber(int number) {
             
-            string[] numerals = new string[] { "I","II","III","IV","V","VI","VII","VIII","IX","X","XX","XXX","XL","L","LX","LXX","LXXX","XC","C" };
+            string[] numerals = new string[] {"", "I","II","III","IV","V","VI","VII","VIII","IX","","X","XX","XXX","XL","L","LX","LXX","LXXX","XC","C" };
             int units, tens;
             string numberConverted;
                 numberConverted = "";
             units = number % 10;
             tens = number / 10;
-            numberConverted = numerals[8 + tens];
-            numberConverted = numberConverted + numerals[units - 1];
-
+            numberConverted = numerals[10+ tens];
+            numberConverted = numberConverted + numerals[units];
             return numberConverted;
 
 
